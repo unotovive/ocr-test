b808c773568d067b778df307ae82e8743c0c7665
unotovive <tec.unotovive@gmail.com>
2021-11-09 10:32:19 +0900

    feat: add OCR test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <video ref="videoRef" autoPlay style="width: 100%" />
+      <pre>
+        <h1>{{ text }}</h1>
+      </pre>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { createWorker } from "tesseract.js";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      worker: null,
+      stream: null,
+      text: "",
+    };
+  },
+  async mounted() {
+    const worker = createWorker();
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    this.worker = worker;
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { width: 1000, height: 1000 },
+      audio: false,
+    });
+    this.stream = stream;
+  },
+  watch: {
+    worker() {
+      if (this.worker && this.stream && this.$refs.videoRef.current !== null) {
+        this.$refs.videoRef.srcObject = this.stream;
+        const clear = this.onRecognizeText();
+        return clear;
+      }
+    },
+    stream() {
+      if (this.worker && this.stream && this.$refs.videoRef.current !== null) {
+        this.$refs.videoRef.srcObject = this.stream;
+        const clear = this.onRecognizeText();
+        return clear;
+      }
+    },
+  },
+  methods: {
+    onRecognizeText() {
+      const timerId = setInterval(async () => {
+        if (this.$refs.videoRef === null || !this.worker) return;
+
+        const c = document.createElement("canvas");
+        c.width = 1000;
+        c.height = 1000;
+        c.getContext("2d")?.drawImage(this.$refs.videoRef, 0, 0, 1000, 1000);
+        const {
+          data: { text },
+        } = await this.worker.recognize(c);
+        this.text = text;
+      }, 500);
+      return () => clearInterval(timerId);
+    },
   },
 };
 </script>
